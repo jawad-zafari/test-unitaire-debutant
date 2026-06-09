0ca80eeac31e113938f72c2d4ca72642da6ac65f
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-09 11:32:03 +0200

    Ajouter un test unitaire pour la méthode d'addition de la classe Math

--- jour02-job02/tests/MathTest.php
+++ jour02-job02/tests/MathTest.php
@@ -4,3 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../src/Math.php';
 
+class MathTest extends TestCase {
+    public function testAddition() {
+        $math = new Math();
+        $this->assertEquals(5, $math->addition(2, 3));
+    }
+}
