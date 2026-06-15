a75953ac6bbc7c89256bef0e7eeb872c5e734cfe
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-15 22:39:10 +0200

    Ajouter un test pour l'addition de deux nombres

--- jour03-job03/tests/calculator.test.js
+++ jour03-job03/tests/calculator.test.js
@@ -2,5 +2,9 @@ const { calculate } = require('../calculator.js');
 
 describe('Tests de la calculatrice JavaScript', () => {
     
+    test('Addition : 2 + 3 = 5', () => {
+        expect(calculate('2+3')).toBe(5);
+    });
+
     
 });
